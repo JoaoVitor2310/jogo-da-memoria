bf9fa464cafeee0cc6063dacbc226ba6cc021ca6
João Vitor Gouveia <joaovitormatosgouveia@gmail.com>
2022-02-11 13:09:56 -0300

    inicial script

---
+++ script.js
@@ -0,0 +1,14 @@
+const FRONT = "card-front";
+const BACK =  "card-back";
+
+let flags = ['brazil',
+    'argentina',
+    'uruguay',
+    'canada',
+    'chile',
+    'england',
+    'germany',
+    'portugal',
+    'sweeden',
+    'USA'
+]
